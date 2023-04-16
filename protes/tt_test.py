ca8b98d43ca61a6e9bebe14d601938da9ca4a03f
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2023-04-16 14:39:48 +0300

    Fix some tests

--- protes/tt_test.py
+++ protes/tt_test.py
@@ -8,7 +8,21 @@ from protes.tt import TensorTrain, TensorTrainDensity
 
 class TestTensorTrainDensity:
 
-    def test_sample():
+    def test_from_train(self):
+        key = jax.random.PRNGKey(42)
+        cores = [
+            jax.random.uniform(key, (1, 3, 2)),
+            jax.random.uniform(key, (2, 3, 2)),
+            jax.random.uniform(key, (2, 3, 1)),
+        ]
+        train = TensorTrain.from_cores(cores)
+        pdf = jax.jit(TensorTrainDensity.from_train)(train)
+        assert pdf.ndim == 3
+        assert pdf.train.ranks == (1, 2, 2, 1)
+        assert len(pdf.interfaces) == pdf.ndim
+        assert tuple([x.size for x in pdf.interfaces]) == (2, 2, 1)
+
+    def test_sample(self):
         key = jax.random.PRNGKey(42)
         cores = [jax.random.uniform(key, (1, 3, 1)) for _ in range(3)]
         train = TensorTrain.from_cores(cores)
@@ -25,7 +39,7 @@ class TestTensorTrainDensity:
         assert samples.shape[:-1] == shape
         assert samples.shape[-1] == train.ndim
 
-    def test_score():
+    def test_score(self):
         # Generate uniform distribution of indices.
         pdf = TensorTrainDensity.uniform((3, 3, 3), (1, 1, 1, 1))
         assert len(pdf.interfaces) == pdf.ndim
@@ -36,5 +50,6 @@ class TestTensorTrainDensity:
         assert log_probas.ndim == 1
         assert log_probas.shape == indices.shape[:1]
 
-        probas = jnp.exp(log_probas)
+        probas = jnp.exp(log_probas)  # Unnormalized proba.
+        probas /= probas.sum()  # Normalized proba over entire space.
         assert_allclose(probas, 1 / 3**3, atol=1e-6)
